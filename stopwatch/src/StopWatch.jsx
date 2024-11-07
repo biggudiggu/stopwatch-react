ebd6bb5514f60dedcdfe6d83a8aa0cee168f6bc8
biggudiggu <busragci@hotmail.com>
2024-11-07 16:26:08 +0100

    fin

---
+++ stopwatch/src/StopWatch.jsx
@@ -0,0 +1,64 @@
+import React, {useState, useEffect, useRef} from "react"
+
+function StopWatch() {
+
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const intervalIdRef = useRef(null);
+  const startTimeRef = useRef(0);
+
+  useEffect(() => {
+    if (isRunning) {
+      intervalIdRef.current = setInterval(() => {
+        setElapsedTime(Date.now() - startTimeRef.current);
+      }, 10);
+    }
+
+    return() => {
+      clearInterval(intervalIdRef.current);
+    };
+
+  }, [isRunning]);
+
+
+  function start() {
+    setIsRunning(true);
+    startTimeRef.current = Date.now() - elapsedTime;
+  }
+
+  function stop() {
+    setIsRunning(false);
+  }
+
+  function reset() {
+    setElapsedTime(0);
+    setIsRunning(false);
+  }
+
+  function formatTime() {
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 /10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}:${milliseconds}`;
+  }
+  
+  return(
+  <div id="container">
+    <h1>STOPWATCH</h1>
+    <div id="display">{formatTime()}</div>
+    <div id="controls">
+      <button id="startBtn" onClick={start}>Start</button>
+      <button id="stopBtn" onClick={stop}>Stop</button>
+      <button id="resetBtn" onClick={reset}>Reset</button>
+    </div>
+</div>);
+}
+
+export default StopWatch
